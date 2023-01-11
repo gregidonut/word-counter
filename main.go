26cce6523497e907005c4bc2025c3ca3192d5f9d
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-11 14:37:57 +0800

    feat: added bool param to count() function
    added second param to bool function to check if the flag was invoked
    from main() and added logic to do the default behavior of counting words
    if the flag was not supplied

--- main.go
+++ main.go
@@ -19,12 +19,16 @@ func main() {
 	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 
-	// Define the scanner split type to words(default is split by lines)
-	scanner.Split(bufio.ScanWords)
+	// If the count lines flag is not set, we want to count words os we define
+	//the scanner split type to words (default is split by lines)
+	if !countLines {
+		// Define the scanner split type to words(default is split by lines)
+		scanner.Split(bufio.ScanWords)
+	}
 
 	// Defining a counter
 	wc := 0
