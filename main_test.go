d6d4305abe99463db37eca7785ddea5702b69968
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-11 14:08:06 +0800

    test: add initial testing file main_test.go
    added first test file that tests the count() function implemented with a
    simple table test that is admittedly a bit overkill for this specific
    scenario but you know, their cheap, so why not.

---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func Test_count(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "initial",
+			args: args{
+				r: bytes.NewBufferString("word1 word2 word3 word4\n"),
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := count(tt.args.r); got != tt.want {
+				t.Errorf("count() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
