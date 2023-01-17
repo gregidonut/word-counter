1f4836510b0d1ecaf5bbc3364714c010997bdd6f
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-18 00:07:28 +0800

    test: finish test cases for actualRun()
    did some generic testcases not accounting for error of combination use
    cases implemented with a generic table test less of an integration test
    and more of just a regular function test but since the main function is
    essentially just a wrapper around the run function except for the
    errors. it's satisfactory

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"testing"
 )
@@ -46,6 +47,81 @@ func Test_count(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := count(tt.args.r, tt.args.countLines, tt.args.countBytes); got != tt.want {
 				t.Errorf("count() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+const (
+	foxTextFile = "./testdata/fox.txt"
+	jerryMDFile = "./testdata/jerry.md"
+)
+
+func Test_actualRun(t *testing.T) {
+	type args struct {
+		countLines bool
+		countBytes bool
+		files      []string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		wantW string
+	}{
+		{
+			name: "OneFileNoOtherFlags",
+			args: args{
+				files: []string{foxTextFile},
+			},
+			wantW: fmt.Sprintf("%s: %d\ntotal: %d\n", foxTextFile, 9, 9),
+		},
+		{
+			name: "TwoFilesNoOtherFlags",
+			args: args{
+				files: []string{foxTextFile, jerryMDFile},
+			},
+			wantW: fmt.Sprintf("%s: %d\n%s: %d\ntotal: %d\n", foxTextFile, 9, jerryMDFile, 10, 19),
+		},
+		{
+			name: "OneFileCountLines",
+			args: args{
+				files:      []string{foxTextFile},
+				countLines: true,
+			},
+			wantW: fmt.Sprintf("%s: %d\ntotal: %d\n", foxTextFile, 1, 1),
+		},
+		{
+			name: "TwoFilesCountLines",
+			args: args{
+				files:      []string{foxTextFile, jerryMDFile},
+				countLines: true,
+			},
+			wantW: fmt.Sprintf("%s: %d\n%s: %d\ntotal: %d\n", foxTextFile, 1, jerryMDFile, 3, 4),
+		},
+		{
+			name: "OneFileCountBytes",
+			args: args{
+				files:      []string{foxTextFile},
+				countBytes: true,
+			},
+			wantW: fmt.Sprintf("%s: %d\ntotal: %d\n", foxTextFile, 44, 44),
+		},
+		{
+			name: "TwoFilesCountBytes",
+			args: args{
+				files:      []string{foxTextFile, jerryMDFile},
+				countBytes: true,
+			},
+			wantW: fmt.Sprintf("%s: %d\n%s: %d\ntotal: %d\n", foxTextFile, 44, jerryMDFile, 50, 94),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := &bytes.Buffer{}
+			actualRun(w, tt.args.countLines, tt.args.countBytes, tt.args.files)
+
+			if gotW := w.String(); gotW != tt.wantW {
+				t.Errorf("actualRun() gotW = %v, want %v", gotW, tt.wantW)
 			}
 		})
 	}
